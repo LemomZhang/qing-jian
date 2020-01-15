15fa35fd4a5c2c3b67ab6b276af0b3acc717ea50
zlm <lemom211@outlook.com>
2020-01-15 14:47:55 +0800

    commit-1

---
+++ components/like/like.js
@@ -0,0 +1,25 @@
+// components/like/like.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    number:{
+      type:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
