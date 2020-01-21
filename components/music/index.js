1915880138d391fb1aa8fa333151da9e09184097
zlm <lemom211@outlook.com>
2020-01-21 16:03:21 +0800

    完善音乐播放

--- components/music/index.js
+++ components/music/index.js
@@ -7,47 +7,47 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    music:Array,
-    idd:Number,
-    isPlay:Array
+    music: Array,
+    idd: Number,
+    isPlay: Array
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
-    pauseSrc:'img/pause.png',
-    playSrc:'img/play.png',
+    play: [],
+    pauseSrc: 'img/pause.png',
+    playSrc: 'img/play.png',
   },
   /**
    * 组件的方法列表
    */
   methods: {
-    onPlay(e){
+    onPlay(e) {
+      // 播放音乐
+
       let idd = e.currentTarget.dataset.id
-      if (!this.properties.isPlay[idd]){
-        // this.setData({
-        //   isPlay:true
-        // })
-        for(let i=0;i<this.properties.isPlay.length;i++){
-          if(i===idd){
+      if (!this.properties.isPlay[idd]) {
+        for (let i = 0; i < this.properties.isPlay.length; i++) {
+          if (i === idd) {
             this.properties.isPlay[i] = true
-          }else{
+          } else {
             this.properties.isPlay[i] = false
           }
-       
         }
         aud.title = this.properties.music[idd].name
         aud.src = this.properties.music[idd].play_url
-        console.log(this.properties.isPlay)
-      }else{
-        // this.setData({
-        //   isPlay: false
-        // })
+        this.setData({
+          play: this.properties.isPlay
+        })
+      } else {
+        // 暂停音乐
         this.properties.isPlay[idd] = false
-       aud.pause()
-        console.log(this.properties.isPlay)
+        aud.pause()
+        this.setData({
+          play: this.properties.isPlay
+        })
       }
     },
   }
